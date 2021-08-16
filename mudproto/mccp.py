a719fbc7b5269efc9e43531ac66f893f96a0099e
Nick Stockton <nstockton@users.noreply.github.com>
2021-08-15 22:25:06 -0700

    Add the MUD Client Compression Protocol.

---
+++ mudproto/mccp.py
@@ -0,0 +1,104 @@
+"""
+MUD Client Compression protocol.
+"""
+
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+import logging
+import zlib
+from typing import Any, List
+
+# Local Modules:
+from .base import Protocol
+from .telnet_constants import IAC, MCCP1, MCCP2, SB, SE, WILL
+
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+class MCCPMixIn(Protocol):
+	"""An MCCP mix in class for the Telnet protocol."""
+
+	def __init__(self, *args: Any, **kwargs: Any) -> None:
+		super().__init__(*args, **kwargs)  # type: ignore[misc]
+		self._isCompressed: bool = False
+		self._usingMCCp1: bool = False
+		self._usingMCCp2: bool = False
+		self._compressedInputBuffer: bytearray = bytearray()
+		self._decompressor: Any = None
+
+	def on_dataReceived(self, data: bytes) -> None:
+		outputBuffer: List[bytes] = []
+		inputBuffer: bytearray = self._compressedInputBuffer
+		inputBuffer.extend(data)
+		while inputBuffer:
+			if self._isCompressed:
+				# Compressed data:
+				outputBuffer.append(self._decompressor.decompress(inputBuffer))  # type: ignore[misc]
+				inputBuffer.clear()
+				if self._decompressor.unused_data:  # type: ignore[misc]
+					# Uncompressed data following the compressed data, likely due to the server terminating compression.
+					inputBuffer.extend(self._decompressor.unused_data)  # type: ignore[misc]
+					self._isCompressed = False
+					self._usingMCCp1 = False
+					self._usingMCCp2 = False
+					del self._decompressor
+					self._decompressor = None
+					continue  # Process the remaining uncompressed data.
+				break  # inputBuffer is empty, no need to loop again.
+			# Uncompressed data:
+			iacIndex: int = inputBuffer.find(IAC)
+			if iacIndex >= 0:
+				# An IAC byte was found.
+				outputBuffer.append(inputBuffer[:iacIndex])
+				del inputBuffer[:iacIndex]
+				if len(inputBuffer) == 1:
+					# Partial IAC sequence.
+					break
+				elif inputBuffer[1] == SB[0]:
+					seIndex: int = inputBuffer.find(SE)
+					if seIndex < 0 or inputBuffer[seIndex - 1 : seIndex] not in (IAC, WILL):
+						# Partial subnegotiation sequence.
+						break
+					elif inputBuffer[:seIndex] in (IAC + SB + MCCP1 + WILL, IAC + SB + MCCP2 + IAC):
+						# The server enabled compression. Subsequent data will be compressed.
+						self._isCompressed = True
+						self._decompressor = zlib.decompressobj(zlib.MAX_WBITS)
+					else:
+						# We don't care about other subnegotiations, pass it on.
+						outputBuffer.append(inputBuffer[: seIndex + 1])
+					del inputBuffer[: seIndex + 1]
+				else:
+					# We don't care about other IAC sequences, pass it on.
+					outputBuffer.append(inputBuffer[:2])
+					del inputBuffer[:2]
+			else:
+				# No IAC was found.
+				outputBuffer.append(inputBuffer.copy())
+				inputBuffer.clear()
+		if outputBuffer:
+			super().on_dataReceived(b"".join(outputBuffer))
+
+	def on_enableRemote(self, option: bytes) -> bool:
+		if option == MCCP1:
+			# Only enable MCCP1 if MCCP2 is not enabled.
+			self._usingMCCp1 = not self._usingMCCp2
+			return self._usingMCCp1
+		elif option == MCCP2:
+			# Only enable MCCP2 if MCCP1 is not enabled.
+			self._usingMCCp2 = not self._usingMCCp1
+			return self._usingMCCp2
+		return bool(super().on_enableRemote(option))  # type: ignore[misc] # pragma: no cover
+
+	def on_disableRemote(self, option: bytes) -> None:
+		if option in (MCCP1, MCCP2):
+			return None
+		super().on_disableRemote(option)  # type: ignore[misc] # pragma: no cover
---
+++ tests/mudproto/test_mccp.py
@@ -0,0 +1,124 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+from typing import Any, Tuple
+from unittest import TestCase
+from zlib import DEFLATED, MAX_WBITS, Z_DEFAULT_COMPRESSION, Z_FINISH, Z_SYNC_FLUSH, compressobj
+
+# MUD Protocol Modules:
+from mudproto.mccp import MCCPMixIn
+from mudproto.telnet_constants import (
+	COMMAND_BYTES,
+	CR,
+	CR_LF,
+	ECHO,
+	IAC,
+	IAC_IAC,
+	MCCP1,
+	MCCP2,
+	NEGOTIATION_BYTES,
+	NULL,
+	SB,
+	SE,
+	WILL,
+)
+
+
+class Telnet(MCCPMixIn):
+	def on_connectionMade(self) -> None:
+		pass
+
+	def on_connectionLost(self) -> None:
+		pass
+
+
+class TestMCCPMixIn(TestCase):
+	def setUp(self) -> None:
+		self.gameReceives: bytearray = bytearray()
+		self.playerReceives: bytearray = bytearray()
+		self.telnet: Telnet = Telnet(self.gameReceives.extend, self.playerReceives.extend)
+
+	def tearDown(self) -> None:
+		self.telnet.on_connectionLost()
+		del self.telnet
+		self.gameReceives.clear()
+		self.playerReceives.clear()
+
+	def parse(self, data: bytes) -> Tuple[bytes, bytes]:
+		self.telnet.on_dataReceived(data)
+		playerReceives: bytes = bytes(self.playerReceives)
+		self.playerReceives.clear()
+		gameReceives: bytes = bytes(self.gameReceives)
+		self.gameReceives.clear()
+		self.telnet._compressedInputBuffer.clear()
+		return playerReceives, gameReceives
+
+	def testTelnetOn_dataReceived(self) -> None:
+		# Insure uncompressed data is passed on.
+		data: bytes = b"Hello World!"
+		self.assertEqual(self.parse(data + CR_LF), (data + CR_LF, b""))
+		# Note that the IAC is buffered while looking for MCCP negotiations.
+		self.assertEqual(self.parse(data + IAC), (data, b""))
+		self.assertEqual(self.parse(data + CR + IAC), (data + CR, b""))
+		self.assertEqual(self.parse(data + CR + IAC_IAC), (data + CR + IAC_IAC, b""))
+		# IAC command and negotiation sequences:
+		self.assertEqual(self.parse(data + IAC_IAC), (data + IAC_IAC, b""))
+		# IAC + SE outside of subnegotiation.
+		self.assertEqual(self.parse(data + IAC + SE), (data + IAC + SE, b""))
+		self.assertEqual(self.parse(data + IAC + SB), (data, b""))  # Buffered while looking for MCCP.
+		for byte in COMMAND_BYTES:
+			self.assertEqual(self.parse(data + IAC + byte), (data + IAC + byte, b""))
+		for byte in NEGOTIATION_BYTES:
+			self.assertEqual(self.parse(data + IAC + byte), (data + IAC + byte, b""))
+			self.assertEqual(self.parse(data + IAC + byte + ECHO), (data + IAC + byte + ECHO, b""))
+		self.assertEqual(self.parse(data + IAC + NULL), (data + IAC + NULL, b""))
+		# Partial subnegotiation sequences:
+		# Note that these are buffered.
+		self.assertEqual(self.parse(data + IAC + SB + IAC), (data, b""))
+		self.assertEqual(self.parse(data + IAC + SB + b"something"), (data, b""))
+		# Complete subnegotiations:
+		self.assertEqual(
+			self.parse(data + IAC + SB + ECHO + b"something" + IAC + SE),
+			(data + IAC + SB + ECHO + b"something" + IAC + SE, b""),
+		)
+		self.assertEqual(
+			self.parse(data + IAC + SB + ECHO + b"something" + IAC_IAC + IAC + SE),
+			(data + IAC + SB + ECHO + b"something" + IAC_IAC + IAC + SE, b""),
+		)
+		# Insure compression is enabled on our end when enabled by the server.
+		self.assertFalse(self.telnet._isCompressed)
+		self.assertEqual(self.parse(data + IAC + SB + MCCP1 + WILL + SE), (data, b""))
+		self.assertTrue(self.telnet._isCompressed)
+		self.telnet._isCompressed = False
+		self.assertEqual(self.parse(data + IAC + SB + MCCP2 + IAC + SE), (data, b""))
+		self.assertTrue(self.telnet._isCompressed)
+		# Insure we can actually decompress the data.
+		compressor: Any = compressobj(Z_DEFAULT_COMPRESSION, DEFLATED, MAX_WBITS)
+		compressed: bytes = compressor.compress(data)
+		self.assertEqual(self.parse(compressed + compressor.flush(Z_SYNC_FLUSH)), (data, b""))
+		# Insure that decompression is disabled when Z_FINISH is sent by the server.
+		self.assertEqual(self.parse(compressor.flush(Z_FINISH) + data), (data, b""))
+		self.assertFalse(self.telnet._isCompressed)
+
+	def testTelnetOn_enableRemote(self) -> None:
+		self.assertFalse(self.telnet._usingMCCp1)
+		self.assertFalse(self.telnet._usingMCCp2)
+		# MCCP1 is only allowed if MCCP2 wasn't previously negotiated.
+		self.assertTrue(self.telnet.on_enableRemote(MCCP1))
+		self.assertFalse(self.telnet.on_enableRemote(MCCP2))
+		self.telnet._usingMCCp1 = False
+		# MCCP2 is only allowed if MCCP1 wasn't previously negotiated.
+		self.assertTrue(self.telnet.on_enableRemote(MCCP2))
+		self.assertFalse(self.telnet.on_enableRemote(MCCP1))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	def testTelnetOn_disableRemote(self) -> None:
+		self.telnet.on_disableRemote(MCCP1)  # Should not throw an exception.
+		self.telnet.on_disableRemote(MCCP2)  # Should not throw an exception.
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
