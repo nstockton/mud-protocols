f5be50e63572baaf651443dede19fb3f97a8109c
Nick Stockton <nstockton@users.noreply.github.com>
2021-09-12 05:09:29 -0700

    Switch to using setup.cfg and auto-generated versions.

--- setup.py
+++ setup.py
@@ -6,58 +6,8 @@
 # Future Modules:
 from __future__ import annotations
 
-# Built-in Modules:
-import pathlib
-from typing import List
-
 # Third-party Modules:
-from setuptools import setup  # type: ignore[import]
+from setuptools import setup
 
 
-NAME: str = "mud-protocols"
-DESCRIPTION: str = "MUD protocols implemented in Python."
-KEYWORDS: str = "mud telnet protocol"
-AUTHOR: str = "Nick Stockton"
-AUTHOR_EMAIL: str = "nstockton@users.noreply.github.com"
-VERSION: str = "1.0"
-URL: str = "https://github.com/nstockton/mud-protocols"
-# The directory containing this file
-HERE: pathlib.Path = pathlib.Path(__file__).parent
-README: str = (HERE / "README.md").read_text()
-REQUIREMENTS: List[str] = []
-
-
-setup(
-	python_requires=">=3.7",
-	name=NAME,
-	keywords=KEYWORDS,
-	author=AUTHOR,
-	author_email=AUTHOR_EMAIL,
-	version=VERSION,
-	description=DESCRIPTION,
-	long_description=README,
-	long_description_content_type="text/markdown",
-	url=URL,
-	package_dir={"mudproto": "mudproto"},
-	packages=["mudproto"],
-	package_data={"mudproto": ["py.typed"]},
-	zip_safe=False,
-	setup_requires=REQUIREMENTS,
-	install_requires=REQUIREMENTS,
-	scripts=[],
-	license="Mozilla Public License 2.0 (MPL 2.0)",
-	platforms="Posix; MacOS X; Windows",
-	classifiers=[
-		"License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-		"Programming Language :: Python :: 3 :: Only",
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.7",
-		"Programming Language :: Python :: 3.8",
-		"Programming Language :: Python :: 3.9",
-		"Programming Language :: Python",
-		"Development Status :: 5 - Production/Stable",
-		"Intended Audience :: Developers",
-		"Operating System :: OS Independent",
-		"Topic :: Software Development :: Libraries",
-	],
-)
+setup()
