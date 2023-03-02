6ae99854b00b961457da3ebf0fe076adcf07ce34
Nick Stockton <nstockton@users.noreply.github.com>
2023-03-01 22:40:14 -0800

    Add GMCP support.

---
+++ src/mudproto/gmcp.py
@@ -0,0 +1,242 @@
+"""
+Generic MUD Communication Protocol.
+"""
+
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+import json
+import logging
+import re
+import sys
+from collections.abc import Iterable, Mapping
+from typing import Any, Optional, Union
+
+# Local Modules:
+from .base import Protocol
+from .telnet_constants import GMCP
+
+
+if sys.version_info < (3, 8):  # pragma: no cover
+	from typing_extensions import Protocol as TypeProtocol
+else:  # pragma: no cover
+	from typing import Protocol as TypeProtocol
+
+
+GMCP_MESSAGE_REGEX: re.Pattern[bytes] = re.compile(rb"^\s*(?P<package>[\w.-]+)\s*(?P<value>.*?)\s*$")
+
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+class HasWill(TypeProtocol):
+	def will(self, option: bytes) -> None:
+		...
+
+
+class GMCPMixIn(Protocol, HasWill):
+	"""
+	A GMCP mix in class for the Telnet protocol.
+	"""
+
+	def __init__(
+		self,
+		*args: Any,
+		gmcpClientInfo: Optional[tuple[str, str]] = None,
+		**kwargs: Any,
+	) -> None:
+		"""
+		Defines the constructor for the mixin.
+
+		Args:
+			*args: Positional arguments to be passed to the parent constructor.
+			gmcpClientInfo: A tuple containing client name and version to send peer during GMCP Hello.
+				If left undefined, act as a GMCP server.
+				If defined, act as a GMCP client.
+			**kwargs: Key-word only arguments to be passed to the parent constructor.
+		"""
+		super().__init__(*args, **kwargs)
+		self._gmcpClientInfo: dict[str, str] = {}
+		self._isGMCPClient: bool = False
+		self._isGMCPServer: bool = False
+		if gmcpClientInfo is not None:
+			self._isGMCPClient = True
+			client, version = gmcpClientInfo
+			self._gmcpClientInfo["client"] = client
+			self._gmcpClientInfo["version"] = version
+		else:
+			self._isGMCPServer = True
+		self._isGMCPInitialized: bool = False  # Is set to True after GMCP Hello.
+		self._gmcpSupportedPackages: dict[str, int] = {}  # Keys are lower case.
+		self.subnegotiationMap[GMCP] = self.on_gmcp  # type: ignore[attr-defined]
+
+	@property
+	def isGMCPInitialized(self) -> bool:
+		"""True if GMCP Hello was negotiated, False otherwise."""
+		return self._isGMCPInitialized
+
+	def gmcpSend(self, package: str, value: Any, *, isSerialized: bool = False) -> None:
+		"""
+		Sends a GMCP negotiation to the peer.
+
+		Args:
+			package: The GMCP package name.
+			value: the value to set.
+			isSerialized: True if value has already been serialized to JSON, False otherwise.
+		"""
+		# According to the page at https://gammon.com.au/gmcp
+		# """
+		# Although JSON allows for multiple Unicode encodings, for GMCP all text within
+		# the quotes is encoded in UTF-8 format. We do not recommend the \uXXXX notation
+		# for Unicode characters, as there was some debate about their representation.
+		# """
+		# Because of this, we should pass ensure_ascii=False to json.dumps to prevent unicode escaping.
+		packageAsBytes: bytes = bytes(package, "utf-8")
+		jsonAsBytes: bytes
+		if not isSerialized:
+			jsonStr: str = json.dumps(value, ensure_ascii=False, indent=None, separators=(", ", ": "))
+			jsonAsBytes = bytes(jsonStr, "utf-8")
+		elif isinstance(value, str):
+			jsonAsBytes = bytes(value, "utf-8")
+		else:
+			jsonAsBytes = bytes(value)
+		payload: bytes = b"%b %b" % (packageAsBytes, jsonAsBytes)
+		logger.debug(f"Sending GMCP payload: {payload!r}.")
+		self.requestNegotiation(GMCP, payload)  # type: ignore[attr-defined]
+
+	def gmcpHello(self) -> None:
+		"""
+		Sends a GMCP Hello to the peer.
+		"""
+		logger.debug("Sending GMCP Hello.")
+		self.gmcpSend("Core.Hello", self._gmcpClientInfo)
+
+	def gmcpSetPackages(self, packages: Mapping[str, int]) -> None:
+		"""
+		Tells the peer about supported packages, after clearing any existing list.
+
+		Args:
+			packages: The supported packages.
+		"""
+		self._gmcpSupportedPackages.clear()
+		self._gmcpSupportedPackages.update(
+			{package.lower(): version for package, version in packages.items()}
+		)
+		values: tuple[str, ...] = tuple(f"{package} {version}" for package, version in packages.items())
+		self.gmcpSend("Core.Supports.Set", values)
+
+	def gmcpAddPackages(self, packages: Mapping[str, int]) -> None:
+		"""
+		Tells the peer to append supported packages to an existing list.
+
+		Args:
+			packages: The supported packages.
+		"""
+		self._gmcpSupportedPackages.update(
+			{package.lower(): version for package, version in packages.items()}
+		)
+		values: tuple[str, ...] = tuple(f"{package} {version}" for package, version in packages.items())
+		self.gmcpSend("Core.Supports.Add", values)
+
+	def gmcpRemovePackages(self, packages: Iterable[str]) -> None:
+		"""
+		Tells the peer to remove supported packages.
+
+		Args:
+			packages: The supported packages.
+		"""
+		values: list[str] = []
+		for package in packages:
+			if package.lower() not in self._gmcpSupportedPackages:
+				logger.warning(f"Tried to remove nonexisting package: {package!r}")
+			else:
+				self._gmcpSupportedPackages.pop(package.lower())
+				values.append(package)
+		if values:
+			self.gmcpSend("Core.Supports.Remove", tuple(values))
+
+	def on_gmcpMessage(self, package: str, value: bytes) -> None:
+		"""
+		Called when a GMCP message is received from peer.
+
+		This method should be overridden.
+
+		Args:
+			package: The GMCP package name in lower case.
+			value: The package value.
+		"""
+
+	def on_gmcp(self, data: bytes) -> None:
+		"""
+		Called when a GMCP subnegotiation is received.
+
+		Args:
+			data: The payload.
+		"""
+		match: Union[re.Match[bytes], None] = GMCP_MESSAGE_REGEX.search(data)
+		if match is None:
+			logger.warning(f"Unknown GMCP negotiation from peer: {data!r}")
+			return None
+		package, value = match.groups()
+		logger.debug(f"Received from Peer: GMCP Package: {package!r}, value: {value!r}.")
+		packageAsStr: str = str(package, "utf-8").lower()
+		if self._isGMCPServer:
+			if packageAsStr == "core.hello":
+				if self._isGMCPInitialized:  # GMCP Hello was already received.
+					logger.warning("Received GMCP Hello from peer after initial Hello was already received.")
+				else:  # Initial GMCP Hello.
+					logger.debug("Received initial GMCP Hello from peer.")
+					gmcpClientInfo: dict[str, str] = json.loads(value)
+					self._gmcpClientInfo["client"] = gmcpClientInfo.get("client", "unknown")
+					self._gmcpClientInfo["version"] = gmcpClientInfo.get("version", "0.0")
+					self._isGMCPInitialized = True
+				return None
+			elif not self._isGMCPInitialized:  # Received GMCP before initial Hello.
+				logger.warning("Received GMCP message from peer before initial Hello.")
+		self.on_gmcpMessage(packageAsStr, value)
+
+	def on_connectionMade(self) -> None:
+		super().on_connectionMade()
+		if self._isGMCPServer:
+			logger.debug("We offer to enable GMCP.")
+			self.will(GMCP)
+
+	def on_enableLocal(self, option: bytes) -> bool:
+		if option == GMCP:
+			logger.debug("We enable GMCP.")
+			return True
+		return bool(super().on_enableLocal(option))  # type: ignore[misc] # pragma: no cover
+
+	def on_disableLocal(self, option: bytes) -> None:
+		if option == GMCP:
+			logger.debug("We disable GMCP.")
+			return None
+		super().on_disableLocal(option)  # type: ignore[misc] # pragma: no cover
+
+	def on_enableRemote(self, option: bytes) -> bool:
+		if option == GMCP:
+			logger.debug("Peer enables GMCP.")
+			return True
+		return bool(super().on_enableRemote(option))  # type: ignore[misc] # pragma: no cover
+
+	def on_disableRemote(self, option: bytes) -> None:
+		if option == GMCP:
+			logger.debug("Peer disables GMCP.")
+			return None
+		super().on_disableRemote(option)  # type: ignore[misc] # pragma: no cover
+
+	def on_optionEnabled(self, option: bytes) -> None:
+		if option == GMCP:
+			if self._isGMCPClient:
+				# Hello should be the first thing sent before further GMCP negotiation.
+				self.gmcpHello()
+				self._isGMCPInitialized = True
+			return None
+		super().on_optionEnabled(option)  # type: ignore[misc] # pragma: no cover
---
+++ tests/mudproto/test_gmcp.py
@@ -0,0 +1,303 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+import json
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+# MUD Protocol Modules:
+from mudproto.gmcp import GMCPMixIn
+from mudproto.gmcp import logger as gmcpLogger
+from mudproto.telnet import TelnetProtocol
+from mudproto.telnet_constants import GMCP
+
+
+class Telnet(GMCPMixIn, TelnetProtocol):
+	"""
+	Telnet protocol with GMCP support.
+	"""
+
+
+class TestGMCPMixIn(TestCase):
+	def setUp(self) -> None:
+		self.gameReceives: bytearray = bytearray()
+		self.playerReceives: bytearray = bytearray()
+		self.telnetClient: Telnet = Telnet(
+			self.gameReceives.extend,
+			self.playerReceives.extend,
+			gmcpClientInfo=("test", "1.0"),
+		)
+		self.telnetServer: Telnet = Telnet(
+			self.gameReceives.extend,
+			self.playerReceives.extend,
+		)
+
+	def tearDown(self) -> None:
+		self.telnetClient.on_connectionLost()
+		del self.telnetClient
+		self.telnetServer.on_connectionLost()
+		del self.telnetServer
+		self.gameReceives.clear()
+		self.playerReceives.clear()
+
+	@patch("mudproto.telnet.TelnetProtocol.requestNegotiation")
+	def test_gmcpSend_when_value_is_object(self, mockRequestNegotiation: Mock) -> None:
+		package: str = "Core.Hello"
+		jsonValue: str = json.dumps(
+			self.telnetClient._gmcpClientInfo, ensure_ascii=False, indent=None, separators=(", ", ": ")
+		)
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetClient.gmcpSend("Core.Hello", self.telnetClient._gmcpClientInfo)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockRequestNegotiation.assert_called_once_with(GMCP, payload)
+
+	@patch("mudproto.telnet.TelnetProtocol.requestNegotiation")
+	def test_gmcpSend_when_value_is_string(self, mockRequestNegotiation: Mock) -> None:
+		package: str = "Core.Hello"
+		jsonValue: str = json.dumps(
+			self.telnetClient._gmcpClientInfo, ensure_ascii=False, indent=None, separators=(", ", ": ")
+		)
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetClient.gmcpSend("Core.Hello", jsonValue, isSerialized=True)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockRequestNegotiation.assert_called_once_with(GMCP, payload)
+
+	@patch("mudproto.telnet.TelnetProtocol.requestNegotiation")
+	def test_gmcpSend_when_value_is_bytes(self, mockRequestNegotiation: Mock) -> None:
+		package: str = "Core.Hello"
+		jsonValue: str = json.dumps(
+			self.telnetClient._gmcpClientInfo, ensure_ascii=False, indent=None, separators=(", ", ": ")
+		)
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetClient.gmcpSend("Core.Hello", bytes(jsonValue, "utf-8"), isSerialized=True)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockRequestNegotiation.assert_called_once_with(GMCP, payload)
+
+	@patch("mudproto.gmcp.GMCPMixIn.gmcpSend")
+	def test_gmcpHello(self, mockGMCPSend: Mock) -> None:
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetClient.gmcpHello()
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockGMCPSend.assert_called_once_with("Core.Hello", self.telnetClient._gmcpClientInfo)
+
+	@patch("mudproto.gmcp.GMCPMixIn.gmcpSend")
+	def test_gmcpSetPackages(self, mockGMCPSend: Mock) -> None:
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 0)
+		packages: dict[str, int] = {"Char": 1, "Room": 1}
+		self.telnetClient.gmcpSetPackages(packages)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 2)
+		mockGMCPSend.assert_called_once_with("Core.Supports.Set", ("Char 1", "Room 1"))
+		mockGMCPSend.reset_mock()
+		# Sending set after previously called should clear old values before adding new values.
+		newPackages: dict[str, int] = {"Char": 2, "Client": 1, "External": 1}
+		self.telnetClient.gmcpSetPackages(newPackages)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 3)
+		mockGMCPSend.assert_called_once_with("Core.Supports.Set", ("Char 2", "Client 1", "External 1"))
+
+	@patch("mudproto.gmcp.GMCPMixIn.gmcpSend")
+	def test_gmcpAddPackages(self, mockGMCPSend: Mock) -> None:
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 0)
+		packages: dict[str, int] = {"Char": 1, "Room": 1}
+		self.telnetClient.gmcpAddPackages(packages)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 2)
+		mockGMCPSend.assert_called_once_with("Core.Supports.Add", ("Char 1", "Room 1"))
+		mockGMCPSend.reset_mock()
+		# Sending Add after previously called should append/update values without clearing existing values.
+		newPackages: dict[str, int] = {"Char": 2, "Client": 1, "External": 1}
+		self.telnetClient.gmcpAddPackages(newPackages)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 4)
+		self.assertEqual(self.telnetClient._gmcpSupportedPackages["char"], 2)
+		mockGMCPSend.assert_called_once_with("Core.Supports.Add", ("Char 2", "Client 1", "External 1"))
+
+	@patch("mudproto.gmcp.GMCPMixIn.gmcpSend")
+	def test_gmcpRemovePackages_when_existing_value(self, mockGMCPSend: Mock) -> None:
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 0)
+		self.telnetClient._gmcpSupportedPackages["char"] = 1
+		self.telnetClient._gmcpSupportedPackages["external"] = 1
+		self.telnetClient._gmcpSupportedPackages["room"] = 1
+		self.telnetClient.gmcpRemovePackages(("Char", "External"))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 1)
+		mockGMCPSend.assert_called_once_with("Core.Supports.Remove", ("Char", "External"))
+
+	@patch("mudproto.gmcp.GMCPMixIn.gmcpSend")
+	def test_gmcpRemovePackages_when_both_existing_and_nonexisting_values(self, mockGMCPSend: Mock) -> None:
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 0)
+		self.telnetClient._gmcpSupportedPackages["char"] = 1
+		self.telnetClient._gmcpSupportedPackages["external"] = 1
+		self.telnetClient._gmcpSupportedPackages["room"] = 1
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetClient.gmcpRemovePackages(("Char", "**junk**"))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 2)
+		mockGMCPSend.assert_called_once_with("Core.Supports.Remove", ("Char",))
+
+	@patch("mudproto.gmcp.GMCPMixIn.gmcpSend")
+	def test_gmcpRemovePackages_when_not_existing_value(self, mockGMCPSend: Mock) -> None:
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 0)
+		self.telnetClient._gmcpSupportedPackages["char"] = 1
+		self.telnetClient._gmcpSupportedPackages["external"] = 1
+		self.telnetClient._gmcpSupportedPackages["room"] = 1
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetClient.gmcpRemovePackages(("**junk**",))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(len(self.telnetClient._gmcpSupportedPackages), 3)
+		mockGMCPSend.assert_not_called()
+
+	@patch("mudproto.gmcp.GMCPMixIn.on_gmcpMessage")
+	def test_on_gmcp(self, mockOn_gmcpMessage: Mock) -> None:
+		# Test invalid data.
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetClient.on_gmcp(b"**junk**")
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_not_called()
+		# Test valid data.
+		package: str = "Char.Name"
+		value: dict[str, str] = {"name": "Gandalf", "fullname": "Gandalf the Grey"}
+		jsonValue: str = json.dumps(value, ensure_ascii=False, indent=None, separators=(", ", ": "))
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetClient.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_called_once_with(package.lower(), bytes(jsonValue, "utf-8"))
+		mockOn_gmcpMessage.reset_mock()
+		# Test received valid data, but before initial GMCP Hello as server.
+		self.assertFalse(self.telnetServer.isGMCPInitialized)
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetServer.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_called_once_with(package.lower(), bytes(jsonValue, "utf-8"))
+		mockOn_gmcpMessage.reset_mock()
+		# Test received initial GMCP Hello as server.
+		package = "Core.Hello"
+		jsonValue = json.dumps(
+			self.telnetClient._gmcpClientInfo, ensure_ascii=False, indent=None, separators=(", ", ": ")
+		)
+		payload = bytes(f"{package} {jsonValue}", "utf-8")
+		self.assertFalse(self.telnetServer.isGMCPInitialized)
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetServer.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertTrue(self.telnetServer.isGMCPInitialized)
+		mockOn_gmcpMessage.assert_not_called()
+		# Test received a second GMCP Hello as server.
+		self.assertTrue(self.telnetServer.isGMCPInitialized)
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetServer.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_not_called()
+
+	@patch("mudproto.gmcp.GMCPMixIn.on_gmcpMessage")
+	def test_on_gmcp_when_invalid_data(self, mockOn_gmcpMessage: Mock) -> None:
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetClient.on_gmcp(b"**junk**")
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_not_called()
+
+	@patch("mudproto.gmcp.GMCPMixIn.on_gmcpMessage")
+	def test_on_gmcp_when_valid_data(self, mockOn_gmcpMessage: Mock) -> None:
+		package: str = "Char.Name"
+		value: dict[str, str] = {"name": "Gandalf", "fullname": "Gandalf the Grey"}
+		jsonValue: str = json.dumps(value, ensure_ascii=False, indent=None, separators=(", ", ": "))
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetClient.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_called_once_with(package.lower(), bytes(jsonValue, "utf-8"))
+
+	@patch("mudproto.gmcp.GMCPMixIn.on_gmcpMessage")
+	def test_on_gmcp_when_valid_data_before_initial_hello(self, mockOn_gmcpMessage: Mock) -> None:
+		# This might only happen if acting as GMCP server.
+		package: str = "Char.Name"
+		value: dict[str, str] = {"name": "Gandalf", "fullname": "Gandalf the Grey"}
+		jsonValue: str = json.dumps(value, ensure_ascii=False, indent=None, separators=(", ", ": "))
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		self.assertFalse(self.telnetServer.isGMCPInitialized)
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetServer.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_called_once_with(package.lower(), bytes(jsonValue, "utf-8"))
+
+	@patch("mudproto.gmcp.GMCPMixIn.on_gmcpMessage")
+	def test_on_gmcp_when_received_initial_hello(self, mockOn_gmcpMessage: Mock) -> None:
+		# This might only happen if acting as GMCP server.
+		package: str = "Core.Hello"
+		jsonValue: str = json.dumps(
+			self.telnetClient._gmcpClientInfo, ensure_ascii=False, indent=None, separators=(", ", ": ")
+		)
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		self.assertFalse(self.telnetServer.isGMCPInitialized)
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetServer.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertTrue(self.telnetServer.isGMCPInitialized)
+		mockOn_gmcpMessage.assert_not_called()
+
+	@patch("mudproto.gmcp.GMCPMixIn.on_gmcpMessage")
+	def test_on_gmcp_when_received_multiple_hellos(self, mockOn_gmcpMessage: Mock) -> None:
+		# This might only happen if acting as GMCP server.
+		package: str = "Core.Hello"
+		jsonValue: str = json.dumps(
+			self.telnetClient._gmcpClientInfo, ensure_ascii=False, indent=None, separators=(", ", ": ")
+		)
+		payload: bytes = bytes(f"{package} {jsonValue}", "utf-8")
+		self.assertFalse(self.telnetServer.isGMCPInitialized)
+		self.telnetServer._isGMCPInitialized = True  # Indicate that Hello was previously received.
+		with self.assertLogs(gmcpLogger, "WARNING"):
+			self.telnetServer.on_gmcp(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockOn_gmcpMessage.assert_not_called()
+
+	@patch("mudproto.telnet.TelnetProtocol.will")
+	def test_on_connectionMade_when_acting_as_server(self, mockWill: Mock) -> None:
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetServer.on_connectionMade()
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockWill.assert_called_once_with(GMCP)
+
+	@patch("mudproto.telnet.TelnetProtocol.will")
+	def test_on_connectionMade_when_acting_as_client(self, mockWill: Mock) -> None:
+		self.telnetClient.on_connectionMade()
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockWill.assert_not_called()
+
+	def test_on_enableLocal(self) -> None:
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.assertTrue(self.telnetServer.on_enableLocal(GMCP))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	def test_on_disableLocal(self) -> None:
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetServer.on_disableLocal(GMCP)  # Should not throw an exception.
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	def test_on_enableRemote(self) -> None:
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.assertTrue(self.telnetClient.on_enableRemote(GMCP))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	def test_on_disableRemote(self) -> None:
+		with self.assertLogs(gmcpLogger, "DEBUG"):
+			self.telnetClient.on_disableRemote(GMCP)  # Should not throw an exception.
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	@patch("mudproto.gmcp.GMCPMixIn.gmcpHello")
+	def test_on_optionEnabled(self, mockGMCPHello: Mock) -> None:
+		self.assertFalse(self.telnetClient.isGMCPInitialized)
+		self.telnetClient.on_optionEnabled(GMCP)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockGMCPHello.assert_called_once()
+		self.assertTrue(self.telnetClient.isGMCPInitialized)
