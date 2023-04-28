db2d48f37db73cc0b2017e2fef5c7caed8e7f959
Nick Stockton <nstockton@users.noreply.github.com>
2023-04-27 23:23:45 -0700

    Add the NAWS (Negotiate Window Size) protocol.

---
+++ src/mudproto/naws.py
@@ -0,0 +1,107 @@
+"""
+Negotiate About Window Sys (NAWS).
+"""
+
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+import logging
+from typing import Any
+
+# Local Modules:
+from .base import Protocol
+from .telnet import BaseTelnetProtocol
+from .telnet_constants import NAWS
+
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+class NAWSMixIn(Protocol, BaseTelnetProtocol):
+	"""
+	A NAWS mix in class for the Telnet protocol.
+	"""
+
+	def __init__(self, *args: Any, **kwargs: Any) -> None:
+		"""
+		Defines the constructor for the mixin.
+
+		Args:
+			*args: Positional arguments to be passed to the parent constructor.
+			**kwargs: Key-word only arguments to be passed to the parent constructor.
+		"""
+		super().__init__(*args, **kwargs)
+		self.subnegotiationMap[NAWS] = self.on_naws
+		self._dimensions: tuple[int, int] = (0, 0)  # Width, height.
+
+	@property
+	def dimensions(self) -> tuple[int, int]:
+		return self._dimensions
+
+	@dimensions.setter
+	def dimensions(self, value: tuple[int, int]) -> None:
+		if value[0] < 0 or value[1] < 0:
+			raise ValueError("Width and height must not be less than 0.")
+		self._dimensions = value
+		if self.isClient:
+			# NAWS is negotiated with 16-bit words.
+			width: bytes = int.to_bytes(value[0], length=2, byteorder="big", signed=False)
+			height: bytes = int.to_bytes(value[1], length=2, byteorder="big", signed=False)
+			payload: bytes = width + height
+			logger.debug(f"Sending NAWS payload: {payload!r}.")
+			self.requestNegotiation(NAWS, payload)
+
+	def on_naws(self, data: bytes) -> None:
+		"""
+		Called when a NAWS subnegotiation is received.
+
+		Args:
+			data: The payload.
+		"""
+		if self.isClient:
+			logger.warning("Received NAWS subnegotiation while running in client mode.")
+		elif len(data) != 4:
+			logger.warning(f"Invalid NAWS sequence: {data!r}.")
+		else:
+			# NAWS is negotiated with 16-bit words.
+			width: int = int.from_bytes(data[:2], byteorder="big", signed=False)
+			height: int = int.from_bytes(data[2:], byteorder="big", signed=False)
+			logger.debug(f"Received window size from peer: width = {width}, height = {height}.")
+			self.dimensions = (width, height)
+
+	def on_connectionMade(self) -> None:
+		super().on_connectionMade()
+		if self.isServer:
+			logger.debug("We ask peer to enable NAWS.")
+			self.do(NAWS)
+
+	def on_enableLocal(self, option: bytes) -> bool:
+		if self.isClient and option == NAWS:
+			logger.debug("We enable NAWS.")
+			return True
+		return bool(super().on_enableLocal(option))  # pragma: no cover
+
+	def on_disableLocal(self, option: bytes) -> None:
+		if self.isClient and option == NAWS:
+			logger.debug("We disable NAWS.")
+			return None
+		super().on_disableLocal(option)  # pragma: no cover
+
+	def on_enableRemote(self, option: bytes) -> bool:
+		if self.isServer and option == NAWS:
+			logger.debug("Peer enables NAWS.")
+			return True
+		return bool(super().on_enableRemote(option))  # pragma: no cover
+
+	def on_disableRemote(self, option: bytes) -> None:
+		if self.isServer and option == NAWS:
+			logger.debug("Peer disables NAWS.")
+			return None
+		super().on_disableRemote(option)  # pragma: no cover
---
+++ tests/mudproto/test_naws.py
@@ -0,0 +1,133 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+# Future Modules:
+from __future__ import annotations
+
+# Built-in Modules:
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+# MUD Protocol Modules:
+from mudproto.naws import NAWSMixIn
+from mudproto.naws import logger as nawsLogger
+from mudproto.telnet import TelnetProtocol
+from mudproto.telnet_constants import NAWS
+
+
+class Telnet(NAWSMixIn, TelnetProtocol):
+	"""
+	Telnet protocol with NAWS support.
+	"""
+
+
+class TestNAWSMixIn(TestCase):
+	def setUp(self) -> None:
+		self.gameReceives: bytearray = bytearray()
+		self.playerReceives: bytearray = bytearray()
+		self.telnetClient: Telnet = Telnet(
+			self.gameReceives.extend,
+			self.playerReceives.extend,
+			isClient=True,
+		)
+		self.telnetServer: Telnet = Telnet(
+			self.gameReceives.extend,
+			self.playerReceives.extend,
+			isClient=False,
+		)
+
+	def tearDown(self) -> None:
+		del self.telnetClient
+		del self.telnetServer
+		self.gameReceives.clear()
+		self.playerReceives.clear()
+
+	@patch("mudproto.telnet.TelnetProtocol.requestNegotiation")
+	def test_dimensions_set_when_invalid_value(self, mockRequestNegotiation: Mock) -> None:
+		self.assertEqual(self.telnetClient.dimensions, (0, 0))
+		with self.assertRaises(ValueError):
+			self.telnetClient.dimensions = (-1, 0)
+		with self.assertRaises(ValueError):
+			self.telnetClient.dimensions = (0, -1)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(self.telnetClient.dimensions, (0, 0))
+		mockRequestNegotiation.assert_not_called()
+
+	@patch("mudproto.telnet.TelnetProtocol.requestNegotiation")
+	def test_dimensions_set_when_running_as_client(self, mockRequestNegotiation: Mock) -> None:
+		dimensions: tuple[int, int] = (80, 25)
+		payload: bytes = b"\x00\x50\x00\x19"
+		self.assertEqual(self.telnetClient.dimensions, (0, 0))
+		with self.assertLogs(nawsLogger, "DEBUG"):
+			self.telnetClient.dimensions = dimensions
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(self.telnetClient.dimensions, dimensions)
+		mockRequestNegotiation.assert_called_once_with(NAWS, payload)
+
+	@patch("mudproto.telnet.TelnetProtocol.requestNegotiation")
+	def test_dimensions_set_when_running_as_server(self, mockRequestNegotiation: Mock) -> None:
+		dimensions: tuple[int, int] = (80, 25)
+		self.assertEqual(self.telnetServer.dimensions, (0, 0))
+		with self.assertNoLogs(nawsLogger, "DEBUG"):
+			self.telnetServer.dimensions = dimensions
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(self.telnetServer.dimensions, dimensions)
+		mockRequestNegotiation.assert_not_called()
+
+	def test_on_naws_when_running_as_client(self) -> None:
+		payload: bytes = b"\x00\x50\x00\x19"
+		with self.assertLogs(nawsLogger, "WARNING"):
+			self.telnetClient.on_naws(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(self.telnetClient.dimensions, (0, 0))
+
+	def test_on_naws_when_running_as_server_and_invalid_data(self) -> None:
+		with self.assertLogs(nawsLogger, "WARNING"):
+			self.telnetServer.on_naws(b"**junk**")
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(self.telnetServer.dimensions, (0, 0))
+
+	def test_on_naws_when_running_as_server_and_valid_data(self) -> None:
+		dimensions: tuple[int, int] = (80, 25)
+		payload: bytes = b"\x00\x50\x00\x19"
+		self.assertEqual(self.telnetServer.dimensions, (0, 0))
+		with self.assertLogs(nawsLogger, "DEBUG"):
+			self.telnetServer.on_naws(payload)
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		self.assertEqual(self.telnetServer.dimensions, dimensions)
+
+	@patch("mudproto.telnet.TelnetProtocol.do")
+	def test_on_connectionMade_when_acting_as_client(self, mockDo: Mock) -> None:
+		with self.assertNoLogs(nawsLogger, "DEBUG"):
+			self.telnetClient.on_connectionMade()
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockDo.assert_not_called()
+
+	@patch("mudproto.telnet.TelnetProtocol.do")
+	def test_on_connectionMade_when_acting_as_server(self, mockDo: Mock) -> None:
+		with self.assertLogs(nawsLogger, "DEBUG"):
+			self.telnetServer.on_connectionMade()
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+		mockDo.assert_called_once_with(NAWS)
+
+	def test_on_enableLocal(self) -> None:
+		with self.assertLogs(nawsLogger, "DEBUG"):
+			self.assertTrue(self.telnetClient.on_enableLocal(NAWS))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	def test_on_disableLocal(self) -> None:
+		with self.assertLogs(nawsLogger, "DEBUG"):
+			self.telnetClient.on_disableLocal(NAWS)  # Should not throw an exception.
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	def test_on_enableRemote(self) -> None:
+		with self.assertLogs(nawsLogger, "DEBUG"):
+			self.assertTrue(self.telnetServer.on_enableRemote(NAWS))
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
+
+	def test_on_disableRemote(self) -> None:
+		with self.assertLogs(nawsLogger, "DEBUG"):
+			self.telnetServer.on_disableRemote(NAWS)  # Should not throw an exception.
+		self.assertEqual((self.playerReceives, self.gameReceives), (b"", b""))
