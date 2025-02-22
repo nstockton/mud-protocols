a381490a5ded9eb1d322fe9354853fe9516f2c59
Nick Stockton <nstockton@users.noreply.github.com>
2025-02-22 01:49:00 -0800

    Add copyright line to top of source files.

--- src/mudproto/charset.py
+++ src/mudproto/charset.py
@@ -1,9 +1,9 @@
-"""Charset protocol."""
-
-
+# Copyright (c) 2025 Nick Stockton
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Charset protocol."""
 
 # Future Modules:
 from __future__ import annotations
